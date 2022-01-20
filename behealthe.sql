d198e612b54c8735742ce1155fb2cbaf808f2d4c
ClemChpt <92731734+ClemChpt@users.noreply.github.com>
2022-01-21 00:28:48 +0100

    bdd

--- behealthe.sql
+++ behealthe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 10 jan. 2022 à 13:59
+-- Généré le : jeu. 20 jan. 2022 à 23:27
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.4.9
 
@@ -78,15 +78,23 @@ CREATE TABLE IF NOT EXISTS `user` (
   `prenom` text NOT NULL,
   `dateNaissance` date NOT NULL,
   `email` text NOT NULL,
-  `password` text NOT NULL,
-  `remarque` longtext NOT NULL,
+  `mdp` text NOT NULL,
+  `remarque` longtext,
   `adresse` varchar(255) DEFAULT NULL,
   `ville` varchar(128) DEFAULT NULL,
   `gender` varchar(128) DEFAULT NULL,
   `pays` varchar(128) DEFAULT NULL,
   `postalCode` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `is_admin`, `nom`, `prenom`, `dateNaissance`, `email`, `mdp`, `remarque`, `adresse`, `ville`, `gender`, `pays`, `postalCode`) VALUES
+(1, 0, 'Alice', 'Dupont', '2004-01-01', 'test@test.com', 'motdepasse123', NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 0, 'Doe', 'John', '2013-05-12', 'jonh1@doe.en', 'aze', 'j\'ai mon chapeau et ma pipe', '221 baker street', 'London', NULL, 'UK', 75000);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
